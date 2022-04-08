aaf35a0fa45229b1cdf8f9e0863fa7216efb6a25
Exib1t <dmaznyak2604@gmail.com>
2022-04-08 17:31:37 +0300

    added num2txtchanger

---
+++ numeral_2txt_changer/script.js
@@ -0,0 +1,28 @@
+const str = askString()
+const fil = numeralsToText(str)
+
+//FUNCTIONS//
+
+function askString() {
+    return prompt('Enter string: ')
+}
+
+function numeralsToText(string) {
+    const strList = string.split(' ');
+    strList.filter((value, index) => {
+        switch(value) {
+            case "0" : strList.splice(index, 1, "zero"); break;
+            case "1" : strList.splice(index, 1, "one"); break;
+            case "2" : strList.splice(index, 1, "two"); break;
+            case "3" : strList.splice(index, 1, "three"); break;
+            case "4" : strList.splice(index, 1, "four"); break;
+            case "5" : strList.splice(index, 1, "five"); break;
+            case "6" : strList.splice(index, 1, "six"); break;
+            case "7" : strList.splice(index, 1, "seven"); break;
+            case "8" : strList.splice(index, 1, "eight"); break;
+            case "9" : strList.splice(index, 1, "nine"); break;
+            default: break;
+        }
+    })
+    return strList;
+}
