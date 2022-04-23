33c4013e942454e9729278d4c152fb87cf45f8c6
Exib1t <dmaznyak2604@gmail.com>
2022-04-23 13:02:46 +0300

    minesweeper 1.4

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,108 +1,142 @@
-const board = document.querySelector('#board')
-class Game{
-  constructor(minePath, rowsAmount, clearPath, coverPath) {
+class GameLogic {
+
+  constructor(minePath, clearPath, coverPath, rowsAmount, element) {
     this.minePath = minePath;
-    this.rowsAmount = rowsAmount;
     this.clearPath = clearPath;
     this.coverPath = coverPath;
+    this.rowsAmount = rowsAmount;
+    this.board = document.querySelector(element)
   }
 
-  get createMine() {
-    return `<img src=${this.minePath}></img>`
+  createMine() {
+    return `<img src=${this.minePath} type='mine'>`
   }
 
-  get createClear() {
-    return `<img src=${this.clearPath}></img>`
+  createClear() {
+    return `<img src=${this.clearPath} type='clear'>`
   }
 
-  get createCover() {
-    return `<img src=${this.coverPath}></img>`
+  createCover() {
+    return `<img src=${this.coverPath} type='cover'>`
   }
 
-  get minesLoad() {
-    for (let i = 1; i <= this.rowsAmount; i++) {
-      if(Math.random() < 0.5) {
-        board.insertAdjacentHTML('beforeend', this.createMine)
-        board.insertAdjacentHTML('beforeend', this.createClear)
+  createLoseText() {
+    return '<p>You Lose! Try Again?</p>'
+  }
+
+  randomizeBoard() {
+    this.board.innerHTML = ''
+    this.board.style.filter = 'blur(0px)'
+
+    for (let i = 0; i < this.rowsAmount; i++) {
+      if (Math.random() < 0.5) {
+        this.board.insertAdjacentHTML('beforeend', this.createMine())
+        this.board.insertAdjacentHTML('beforeend', this.createClear())
       } else {
-        board.insertAdjacentHTML('beforeend', this.createClear)
-        board.insertAdjacentHTML('beforeend', this.createMine)
+        this.board.insertAdjacentHTML('beforeend', this.createClear())
+        this.board.insertAdjacentHTML('beforeend', this.createMine())
       }
     }
-  }
 
-  get minesNumbered() {
-    let imagesArray = document.querySelectorAll('img')
-  }
+    let imagesArray = Array.prototype.slice.call(document.querySelectorAll('img[type]')).reverse()
 
-  get minesCoverSet() {
-    let imagesList = document.querySelectorAll('img')
     let num = 0.5;
-    let imagesArray = Array.prototype.slice.call(imagesList).reverse()
-    for(let img of imagesArray) {
-      let imgSource = img.src;
+
+    for (let img of imagesArray) {
+      img.setAttribute('isClickable', false)
+
       img.setAttribute('number', Math.round(num))
-      img.setAttribute('isOpenned', false)
       num += 0.5
-      if (imgSource.includes('mine.png')) {
-        img.setAttribute('type', 'mine')
-        img.src = this.coverPath
-      } else if (imgSource.includes('right.jpg')) {
-        img.setAttribute('type', 'clear')
-        img.src = this.coverPath
-      } else {alert('Error')}
+
+      img.src = this.coverPath
     }
   }
 
-  get imageClick() {
-    let typeValue;
-    board.addEventListener('click', (event) => {
-      let imagesArray = document.querySelectorAll('img')
-      if (event.target.getAttribute('type') === 'mine') {
-        event.target.src = this.minePath;
-        typeValue = false;
-      } else if (event.target.getAttribute('type') === 'clear') {
-        event.target.src = this.clearPath;
-        typeValue = true;
-        let num = event.target.getAttribute('number')
-        event.target.removeAttribute('number')
-        for(let img of imagesArray) {
-          if (img.getAttribute('number') === num) {
-            img.src = this.minePath
-          }
-        }
-      }
-      if(typeValue === false) {
-        if (confirm('You lose! Try again?')) {
-          board.innerHTML = ''
-          this.minesLoad
-          this.minesCoverSet
-        } else {
-          this.minesCoverOff
-        }
-      }
-    })
+  clickOnCell() {
+    this.board.addEventListener('click', (event) => this.clickOnTarget(event))
   }
 
-  get minesCoverOff() {
-    let imagesArray = document.querySelectorAll('img')
-    for(let img of imagesArray) {
+  clickOnTarget(event) {
+
+    this.clickableRow()
+
+    if (event.target.getAttribute('isClickable') === 'false') {
+      return
+    }
+
+    event.target.setAttribute('isClickable', 'false')
+
+    if (event.target.getAttribute('type') === 'mine') {
+      event.target.src = this.minePath
+      this.openAll()
+      this.showLose()
+    } else if (event.target.getAttribute('type') === 'clear') {
+      event.target.src = this.clearPath
+      this.openPair(event)
+    }
+  }
+
+  openPair(event) {
+    let num = event.target.getAttribute('number')
+    event.target.setAttribute('number', this.rowsAmount + 1)
+
+    let imagesArray = Array.prototype.slice.call(document.querySelectorAll('img[type]')).reverse()
+
+    for (let img of imagesArray) {
+      if (img.getAttribute('number') === num) {
+        if (img.getAttribute('type') === 'mine') {
+          img.src = this.minePath
+        } else if (img.getAttribute('type') === 'clear') {
+          img.src = this.clearPath
+        }
+        img.setAttribute('number', this.rowsAmount + 1)
+        img.setAttribute('isClickable', 'false')
+      }
+    }
+  }
+
+  openAll() {
+    let imagesArray = Array.prototype.slice.call(document.querySelectorAll('img[type]')).reverse()
+
+    for (let img of imagesArray) {
       if (img.getAttribute('type') === 'mine') {
         img.src = this.minePath
+        img.style.filter = 'blur(5px)'
       } else if (img.getAttribute('type') === 'clear') {
         img.src = this.clearPath
-      } else {
-        alert('Error')
+        img.style.filter = 'blur(5px)'
       }
     }
   }
 
-  get load() {
-    this.minesLoad
-    this.minesCoverSet
-    this.imageClick
+  clickableRow() {
+    let imagesArray = Array.prototype.slice.call(document.querySelectorAll('img[type]')).reverse()
+
+    let minNum = this.rowsAmount + 1;
+
+    for (let img of imagesArray) {
+      let number = img.getAttribute('number')
+      if (Number(number) > minNum) {
+        break;
+      } else if (Number(number) <= minNum) {
+        minNum = Number(number)
+      }
+    }
+
+    for (let img of imagesArray) {
+      let number = img.getAttribute('number')
+      if (Number(number) === minNum) {
+        img.setAttribute('isClickable', true)
+      }
+    }
   }
+
+  showLose() {
+    this.board.insertAdjacentHTML('beforeend', this.createLoseText())
+  }
+
 }
 
-let gameLoaded = new Game('images/mine.png', 5, 'images/right.jpg', 'images/cover.jpg')
-gameLoaded.load
+const minesweeper = new GameLogic('images/mine.png', 'images/clear.jpg', 'images/cover.jpg', 5, '#board')
+minesweeper.randomizeBoard()
+minesweeper.clickOnCell()
